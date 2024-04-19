5452fae2c137291778fff4b2ab3d3198048a3c2d
kakaiba1985 <69994455+kakaiba1985@users.noreply.github.com>
2024-04-19 18:17:14 +0800

    Update bsb.sh

--- newcsaio.sh
+++ newcsaio.sh
@@ -11,8 +11,6 @@ PORT_UDP='110';
 PORT_SSL='443';
 API_LINK='https://bsb-technology.cloud/authentication/premium';
 API_KEY='Tknetwork';
-
-
 wget -O autodns "https://raw.githubusercontent.com/BadBoy-Dexter/Socks/server_script/python/efrenauto/autodns" && chmod +x autodns && sed -i -e 's/\r$//' ~/autodns && ./autodns
 
 DOMAIN="$(cat /root/subdomain)"
